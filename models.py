a617f04acfa1932da43b430270ad01ab6a395981
Nafie <warrior259@gmail.com>
2021-11-24 20:06:37 +0300

    rename cheduled_data table

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ import sqlalchemy.orm as orm
 import database
 
 class Schedule(database.Base):
-    __tablename__ = "da"
+    __tablename__ = "scheduled_data"
     id = sql.Column(sql.Integer, primary_key=True)
     date = sql.Column(sql.String)
     message = sql.Column(sql.String)
