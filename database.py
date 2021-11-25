d4a7267bee0d4719df54e80d33d20bf92d202625
Nafie <warrior259@gmail.com>
2021-11-25 16:57:02 +0300

    add sqlie3 connect_args

--- database.py
+++ database.py
@@ -6,6 +6,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = sql.create_engine(DATABASE_URL)
+engine = sql.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = orm.sessionmaker(autocommit=False, autoflush =False, bind=engine)
 Base = declarative.declarative_base()
